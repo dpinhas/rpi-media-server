a2ee8417ee286bbc5a532a7b2fd6f2f6fab0ad92
dpinhas <dpinhas@redhat.com>
2023-01-13 19:21:57 +0200

    Initial commit, add docker, k3s, ansible and monitoring stuff

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# rpi-media-server
+# Raspberry-Pi Media Server
+
+
+## `kubernetes`
+media-server on top of a Kubernetes.
+It can be installed either on any k8s cluster or on an home server like Raspberry Pi with k3s.
+
+Includes the following pods:
+- Jackett
+- Sonarr
+- Radarr
+- Transmission
+- Plex
+
+## `Docker`
+Media server on a simple & single host. Designed to run on Raspberry Pi.
+
+
+Includes the following applications:
+- Jackett
+- Sonarr
+- Radarr
+- Transmission
+- Plex
+- Traefik
---
+++ monitoring/scripts/fetch_last_upgrade.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+for i in $(ls /var/log/apt/history*) ; do
+    if [[ $i == *.log ]] ; then
+        if grep -q upgrade /var/log/apt/history.log ; then
+            echo yes
+            continue
+        fi
+    else
+        last_upgrade=$(zcat $i  | grep upgrade -B1 | tail -2 |grep Start-Date | awk -F ' ' '{print $2}' | sed 's/-//g')
+        break
+    fi
+done
+now=$(date '+%Y%m%d')
+result=$(( ($(date --date="$now" +%s) - $(date --date="$last_upgrade" +%s) )/(60*60*24) ))
+
+echo node_apt_last_upgrade  $result
