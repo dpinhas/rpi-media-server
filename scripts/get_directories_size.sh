7f3b6a34aeafe631523da1c4cbd899fb4ae31352
Dor Pinhas <dor.pinhas1@gmail.com>
2023-10-15 10:44:26 +0300

    Fix the script that creates custom metrics for the library directory sizes

--- scripts/get_directories_size.sh
+++ scripts/get_directories_size.sh
@@ -1,33 +1,65 @@
 #!/usr/bin/env bash
+# Default debug mode is off
+DEBUG=false
 
-# Directory paths to check
+# Static array of directories to check
 DIRECTORIES=(
     "/mnt/data/library/tv"
     "/mnt/data/library/movies"
 )
 
-# Output files for metrics
-OUTPUT_FILES=(
-    "/home/pi/rpi-media-server/monitoring/config/custom_metrics/directory_size_tv.prom"
-    "/home/pi/rpi-media-server/monitoring/config/custom_metrics/directory_size_movies.prom"
-)
+# Function to display usage information
+usage() {
+  echo "Usage: $0 [--debug]"
+  exit 1
+}
 
-# Loop through the directories and their corresponding output files
-for i in "${!DIRECTORIES[@]}"; do
-    DIRECTORY="${DIRECTORIES[$i]}"
-    OUTPUT_FILE="${OUTPUT_FILES[$i]}"
-    
-    echo "# HELP node_directory_size_bytes Disk space used by some directories" > "$OUTPUT_FILE"
-    echo "# TYPE node_directory_size_bytes gauge" >> "$OUTPUT_FILE"
-    
-    while IFS= read -r line; do
-        DIR_NAME=$(echo "$line" | awk -F'/' '{ if ($NF != "" && $NF ~ / /) print $NF }')
-        DIR_SIZE=$(echo "$line" | awk '{ print $1 }')
-        if [ -n "$DIR_NAME" ]; then
-            METRIC_NAME="node_directory_size_bytes"
-            METRIC_VALUE="${DIR_SIZE}"
-            echo "${METRIC_NAME}{directory=\"$DIR_NAME\"} $METRIC_VALUE" >> "$OUTPUT_FILE"
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --debug)
+      DEBUG=true
+      shift
+      ;;
+    -*)
+      echo "Unknown option: $1"
+      usage
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+
+# Loop through the specified directories
+for DIRECTORY in "${DIRECTORIES[@]}"; do
+  if [ ! -d "$DIRECTORY" ]; then
+    echo "Directory not found: $DIRECTORY"
+    continue
+  fi
+
+  # Determine the output file based on the directory
+  OUTPUT_FILE="/home/pi/rpi-media-server/monitoring/config/custom_metrics/directory_size_$(basename "$DIRECTORY").prom"
+
+  echo "# HELP node_directory_size_bytes Disk space used by some directories" > "$OUTPUT_FILE"
+  echo "# TYPE node_directory_size_bytes gauge" >> "$OUTPUT_FILE"
+
+  # Use while read loop to handle lines from 'du'
+  while IFS= read -r line; do
+    if [[ $line =~ ^([0-9]+)[[:space:]]+(.*)$ ]]; then
+      DIR_SIZE="${BASH_REMATCH[1]}"
+      DIR_NAME="${BASH_REMATCH[2]}"
+      METRIC_NAME="node_directory_size_bytes"
+      METRIC_VALUE="${DIR_SIZE}"
+
+      if [ "$DIR_NAME" != "$DIRECTORY" ]; then
+        if [ "$DEBUG" = true ]; then
+          echo "${METRIC_NAME}{directory=\"$DIR_NAME\"} $METRIC_VALUE"
+        else
+          echo "${METRIC_NAME}{directory=\"$DIR_NAME\"} $METRIC_VALUE" >> "$OUTPUT_FILE"
         fi
-    done <<< "$(du --block-size=1 --max-depth=1 "$DIRECTORY")"
+      fi
+    fi
+  done < <(du --block-size=1 --max-depth=1 "$DIRECTORY" 2>/dev/null)
 done
 
